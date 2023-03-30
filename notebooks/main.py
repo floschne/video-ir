f3a17c1a1c5dc26e2e5ce5b6a4486c0f7dcd292f
nargesbh <nargesbabaahmadi7@gmail.com>
2023-03-30 12:51:07 +0200

    the main file of this app

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,15 +1,50 @@
+import find_similarity as fs
+import numpy as np
+import text_embedding 
+
 import gradio as gr
 
 def retrieval(text):
-    return ["Hello " + text + "!", 
-            "https://example.com/video1.mp4",
-            "https://example.com/video2.mp4",
-            "https://example.com/video3.mp4"]
 
-with gr.Interface(fn=retrieval, 
-                  inputs=gr.inputs.Textbox(label="Text", lines=2, placeholder="Text Here..."), 
-                  outputs=[gr.outputs.Video(label="Output 1"), 
+    txt = np.load('/srv/home/ahmadi/gitrepos/video-ir/car_s.npy') 
+    # txt = text_embedding.find_embedding(text)
+
+    arr = fs.find_sim(txt)
+
+    ret_arr = []
+    path = "/srv/home/ahmadi/gitrepos/video-ir/scene_mp4/"
+
+    for i in range(len(arr)):
+        x = arr[i+1][0]
+        s = x.find("-")
+        e = x.find(".")
+        vid_name = x[s+1:e]
+        ret_arr.append(vid_name)
+        path_i = path + vid_name + ".mp4"
+        ret_arr.append(path_i)
+
+    return ret_arr
+
+
+# with gr.Interface(fn=retrieval,
+#                   inputs=gr.inputs.Textbox(label="Text", lines=2, placeholder="Text Here..."),
+#                   outputs=[gr.outputs.Video(label="Output 1"),
+#                            gr.outputs.Video(label="Output 2"),
+#                            gr.outputs.Video(label="Output 3"),
+#                            gr.outputs.Video(label="Output 4")]) as iface:
+#     iface.launch()
+
+
+with gr.Interface(fn=retrieval,
+                  inputs=gr.inputs.Textbox(label="Text", lines=2, placeholder="Text Here..."),
+                  outputs=[gr.outputs.Textbox(label="Output 1"),
+                           gr.outputs.Video(label="Output 1"),
+                           gr.outputs.Textbox(label="Output 2"),
                            gr.outputs.Video(label="Output 2"),
+                           gr.outputs.Textbox(label="Output 3"),
                            gr.outputs.Video(label="Output 3"),
+                           gr.outputs.Textbox(label="Output 4"),
                            gr.outputs.Video(label="Output 4")]) as iface:
     iface.launch()
+
+
