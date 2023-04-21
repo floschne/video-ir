19545132ad8e4f0bdeb6fcaa197c2fd1e5962654
nargesbh <nargesbabaahmadi7@gmail.com>
2023-04-21 14:04:50 +0200

    TRhe final file to make indexes

---
+++ notebooks/build_index.py
@@ -0,0 +1,13 @@
+from autofaiss import build_index
+
+import numpy as np
+
+
+
+
+embeddings_path = "/raid/datasets/msr-vtt/scene_embeddings_14"
+index_folder_path = "/raid/datasets/msr-vtt/index_folder_14"
+
+build_index(embeddings=embeddings_path, index_path= index_folder_path+"/Test_knn.index",
+            index_infos_path=index_folder_path+"/index_infos.json", max_index_memory_usage="2G",
+            current_memory_available="4G")
