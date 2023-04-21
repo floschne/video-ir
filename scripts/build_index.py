ee50fc05a6458db1a0f8504f8b9dbb6cba31d7b7
nargesbh <nargesbabaahmadi7@gmail.com>
2023-04-21 15:22:41 +0200

    cleaned and fire added

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -1,8 +1,10 @@
 from autofaiss import build_index
+import fire
 
-embeddings_path = "/raid/datasets/msr-vtt/scene_embeddings_14"
-index_folder_path = "/raid/datasets/msr-vtt/index_folder_14"
+def main(embeddings_path: str = "/raid/datasets/msr-vtt/scene_embeddings_14", index_folder_path: str = "/raid/datasets/msr-vtt/index_folder_14"):
+    build_index(embeddings=embeddings_path, index_path= index_folder_path+"/Test_knn.index",
+                index_infos_path=index_folder_path+"/index_infos.json", max_index_memory_usage="2G",
+                current_memory_available="4G")
 
-build_index(embeddings=embeddings_path, index_path= index_folder_path+"/Test_knn.index",
-            index_infos_path=index_folder_path+"/index_infos.json", max_index_memory_usage="2G",
-            current_memory_available="4G")
+if __name__ == "__main__":
+    fire.Fire(main)
