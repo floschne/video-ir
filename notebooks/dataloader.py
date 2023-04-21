10959ae657124b627bee187b86482f68e459d570
nargesbh <nargesbabaahmadi7@gmail.com>
2023-04-21 14:03:54 +0200

    made final changes and deleted unecessary files

--- notebooks/dataloader.py
+++ notebooks/dataloader.py
@@ -7,6 +7,8 @@ import glob
 from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence
 import numpy as np
 import math
+import make_scenes_files
+
 
 class VideoDataset(torch.utils.data.Dataset):
     def __init__(self, video_dir):
@@ -15,24 +17,22 @@ class VideoDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         video_path = self.video_paths[idx]
         video, _, info = torchvision.datasets.video_utils.read_video(video_path, pts_unit="sec")
-        return video
+        return video, video_path
 
     def __len__(self):
         return len(self.video_paths)
-
-        return video
+    
 
 def collate_fn(batch):
     # Sort the batch by video length
-    batch.sort(key=lambda x: x.shape[0], reverse=True)
+    batch.sort(key=lambda x: x[0].shape[0], reverse=True)
 
-    # Create a list of tensors representing each video in the batch
-    videos = []
-    for video in batch:
-        if isinstance(video, np.ndarray):
-            videos.append(torch.from_numpy(video))
-        else:
-            videos.append(video)
+    # Extract the videos and their file paths from the tuples
+    videos = [x[0] for x in batch]
+    video_paths = [x[1] for x in batch]
+
+    # Convert numpy arrays to PyTorch tensors
+    videos = [torch.from_numpy(video) if isinstance(video, np.ndarray) else video for video in videos]
 
     # Pack the videos into a single PackedSequence
     packed_videos = pack_sequence(videos)
@@ -40,26 +40,31 @@ def collate_fn(batch):
     # Pad the packed sequence
     padded_videos, _ = pad_packed_sequence(packed_videos, batch_first=True)
 
-    return padded_videos
+    return padded_videos, video_paths
 
 
+p = "/raid/datasets/msr-vtt/TestVideo"
 
-
-dataset = VideoDataset(video_dir="/raid/datasets/msr-vtt/Test2")
-first_data = dataset[0]
+dataset = VideoDataset(video_dir=p)
+# first_data = dataset[0]
 # print(first_data)
 
 dataloader = data.DataLoader(dataset, batch_size=64, shuffle=False, collate_fn=collate_fn)
 
 
+
+# dataiter = iter(dataloader)
+# data1 = dataiter.next()
+# print(data1)
+
 total_samples = len(dataset)
-n_iterations = math.ceil(total_samples/4)
+n_iterations = math.ceil(total_samples/64)
 
-# print(total_samples, n_iterations)
+print(total_samples, n_iterations)
 
-for i, input in enumerate(dataloader):
-    if (i+1) % 2 == 0:
-        print(f' epoch = 1 , step {i+1}/{n_iterations}, inputs {input.shape}')
+batch_size=64
 
-
+for i, batch in enumerate(dataloader):
+    print(batch[1])
+    # make_scenes_files.scene_txt(batch[1], p)
 
--- notebooks/main.py
+++ notebooks/main.py
@@ -1,33 +1,68 @@
-import find_similarity as fs
-import numpy as np
-import text_embedding 
-
+# import numpy as np
+from transformers import AutoTokenizer, AutoModel
+import faiss
+import glob
 import gradio as gr
 
-def retrieval(text):
+def find_text_embedding(text_str, mod_path):
+    tokenizer = AutoTokenizer.from_pretrained(mod_path)
+    model = AutoModel.from_pretrained(mod_path)
 
-    txt = np.load('/srv/home/ahmadi/gitrepos/video-ir/car_s.npy') 
-    # txt = text_embedding.find_embedding(text)
+    inputs = tokenizer([text_str], padding=True, return_tensors="pt")
+    text_features = model.get_text_features(**inputs)
 
-    arr = fs.find_sim(txt)
+    np_arr = text_features.cpu().detach().numpy()
+    return np_arr
 
+
+def find_sim(txt_array, ind_pa, embedding_pa):
+  query_vector = txt_array
+  k = 5
+  index_path = ind_pa +"/Test_knn.index"
+  my_index = faiss.read_index(index_path)
+  distances, indices = my_index.search(query_vector, k)
+
+  dict = {}
+  for i, (dist, indice) in enumerate(zip(distances[0], indices[0])):
+    key_arr = []
+    # Load the file names corresponding to the vector numbers
+    # video_embeddings_folder = "/raid/datasets/msr-vtt/Test_scene_embeddings_"+suffix
+    file_names = glob.glob(embedding_pa +'/*.npy')
+    file_names = sorted(file_names)
+    file_name = file_names[indice].split('/')[-1] 
+    key_arr.append(file_name)
+    key_arr.append(dist)
+    dict[i+1] = key_arr  
+  return dict
+
+def retrieval(text, model):
+    txt = find_text_embedding(text, model)
+
+######only for this application###############################################
+    ind_pa = "/raid/datasets/msr-vtt/index_folder_" + model[-2:]
+    embedding_pa = "/raid/datasets/msr-vtt/scene_embeddings_" + model[-2:]
+    #all_vids
+    path = "/raid/datasets/msr-vtt/all_vids/"
+################################################################################
+
+    arr = find_sim(txt, ind_pa, embedding_pa)
     ret_arr = []
-    path = "/srv/home/ahmadi/gitrepos/video-ir/scene_mp4/"
-
     for i in range(len(arr)):
         x = arr[i+1][0]
-        s = x.find("-")
+        # print(x)
+        #to show all video
+        s = x.rfind("-")
+
         e = x.find(".")
         vid_name = x[s+1:e]
         ret_arr.append(vid_name)
         path_i = path + vid_name + ".mp4"
         ret_arr.append(path_i)
-
     return ret_arr
 
-
 with gr.Interface(fn=retrieval,
-                  inputs=gr.inputs.Textbox(label="Text", lines=2, placeholder="Text Here..."),
+                  inputs=[gr.inputs.Textbox(label="Text", lines=2, placeholder="Text Here..."),
+                          gr.inputs.Dropdown(label="Model", choices=["microsoft/xclip-base-patch32", "microsoft/xclip-base-patch16", "microsoft/xclip-large-patch14"])],
                   outputs=[gr.outputs.Textbox(label="Output 1"),
                            gr.outputs.Video(label="Output 1"),
                            gr.outputs.Textbox(label="Output 2"),
@@ -35,7 +70,6 @@ with gr.Interface(fn=retrieval,
                            gr.outputs.Textbox(label="Output 3"),
                            gr.outputs.Video(label="Output 3"),
                            gr.outputs.Textbox(label="Output 4"),
-                           gr.outputs.Video(label="Output 4")]) as iface:
+                           gr.outputs.Video(label="Output 4")
+                           ]) as iface:
     iface.launch()
-
-
